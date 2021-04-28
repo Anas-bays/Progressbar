f64acbb22094f9807b307e5b8008edafc949d802
Anas-bays <83026708+Anas-bays@users.noreply.github.com>
2021-04-28 17:28:26 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,17 @@
 
+import time,sys
+toolbar = 50
+# setting up toolbar [-------------------------------------]
+sys.stdout.write("[%s]"%(("-")*toolbar))
+sys.stdout.flush()
+# each hash represents 2 % of the progress
+for i in range(toolbar):
+  sys.stdout.write("\r")  # return to start of line
+  sys.stdout.flush()
+  sys.stdout.write("[")  # Overwrite over the existing tex from the start 
+  sys.stdout.write("▮"*(i+1)) # number of ▮ denotes th progress completed 
+  sys.stdout.flush()
+  time.sleep(0.05)
+sys.stdout.write("]")
+
+
