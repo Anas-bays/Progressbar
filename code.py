99f7e303d66a85529b47cc0fca6b62ccd70892c7
Anas-bays <83026708+Anas-bays@users.noreply.github.com>
2021-05-02 14:38:22 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 from time import sleep
 from progress.bar import Bar
 
-with Bar('Progress:', fill='#', suffix='%(percent).1f%% - %(eta)ds') as bar:
+with Bar('Progress:', fill='#', suffix='%(percent).1f%% Complete') as bar:
     for i in range(100):
         sleep(0.02)
         bar.next()
