442d02e5cdc774b26589353895ea8e1f3c7b3598
Anas-bays <83026708+Anas-bays@users.noreply.github.com>
2021-05-06 20:02:13 +0000

    Update code.py

--- code.py
+++ code.py
@@ -4,4 +4,6 @@ from progress.bar import Bar
 with Bar('Progress:', fill='#', suffix='%(percent).1f%% Complete') as bar:
     for i in range(100):
         sleep(0.02)
+        # Do Work here.
         bar.next()
+bar.finish()
